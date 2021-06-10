ad294fcd8c9fe039d23316758cfa9d9e10142a1a
AlexeyR1 <repin.alex2099@yandex.ru>
2021-06-10 23:40:29 +0300

    Update CameraSize.cs

--- Defend Zi/Assets/Scripts/Camera/CameraSize.cs
+++ Defend Zi/Assets/Scripts/Camera/CameraSize.cs
@@ -6,42 +6,51 @@ public class CameraSize : MonoBehaviourExt
     [SerializeField] private Camera _camera;
     [SerializeField] private Transform _playerTransform;
 
+    private Vector3 _leftDownCorner;
+    private Vector3 _rightDownCorner;
+    private Vector3 _rightTopCorner;
+    private Vector3 _leftTopCorner;
     private float _depth;
 
     private void Update()
     {
-        _depth = _playerTransform.position.z - _camera.transform.position.z;
+        UpdateSize();
     }
 
     private void OnDrawGizmos()
     {
-        Vector3 leftDown = GetLeftDownCorner();
-        Vector3 rightDown = GetRightDownCorner();
-        Vector3 rightTop = GetRightTopCorner();
-        Vector3 leftTop = GetLeftTopCorner();
-
-        Gizmos.DrawLine(leftDown, rightDown);
-        Gizmos.DrawLine(rightDown, rightTop);
-        Gizmos.DrawLine(rightTop, leftTop);
-        Gizmos.DrawLine(leftTop, leftDown);
+        Gizmos.DrawLine(_leftDownCorner, _rightDownCorner);
+        Gizmos.DrawLine(_rightDownCorner, _rightTopCorner);
+        Gizmos.DrawLine(_rightTopCorner, _leftTopCorner);
+        Gizmos.DrawLine(_leftTopCorner, _leftDownCorner);
     }
 
-    public Vector3 GetLeftDownCorner()
+    private void UpdateSize()
+    {
+        _leftDownCorner = GetLeftDownCorner();
+        _rightDownCorner = GetRightDownCorner();
+        _rightTopCorner = GetRightTopCorner();
+        _leftTopCorner = GetLeftTopCorner();
+
+        _depth = _playerTransform.position.z - _camera.transform.position.z;
+    }
+
+    private Vector3 GetLeftDownCorner()
     {
         return _camera.ScreenToWorldPoint(new Vector3(0f, 0f, _depth));
     }
 
-    public Vector3 GetRightDownCorner()
+    private Vector3 GetRightDownCorner()
     {
         return _camera.ScreenToWorldPoint(new Vector3(0f, _camera.pixelHeight, _depth));
     }
 
-    public Vector3 GetRightTopCorner()
+    private Vector3 GetRightTopCorner()
     {
         return _camera.ScreenToWorldPoint(new Vector3(_camera.pixelWidth, _camera.pixelHeight, _depth));
     }
 
-    public Vector3 GetLeftTopCorner()
+    private Vector3 GetLeftTopCorner()
     {
         return _camera.ScreenToWorldPoint(new Vector3(_camera.pixelWidth, 0f, _depth));
     }
