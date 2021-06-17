0880c9c3bcff633eb0c384ec077f1cc355b92c51
AlexeyR1 <repin.alex2099@yandex.ru>
2021-06-18 02:19:47 +0300

    Remove counter property

--- Defend Zi/Assets/Scripts/Obstacle/Movement/AroundItsAxisMovement.cs
+++ Defend Zi/Assets/Scripts/Obstacle/Movement/AroundItsAxisMovement.cs
@@ -1,14 +1,7 @@
 ﻿public class AroundItsAxisMovement : RotationMovement
 {
-    protected override void Constructor()
-    {
-        base.Constructor();
-        Counter = Rotation.Value;
-    }
-
     protected override void Move(float deltaTime)
     {
-        Counter += deltaTime * Speed; 
-        Rotation.Rotate(Counter);
+        Rotation.Rotate(Rotation.Value + deltaTime * Speed);
     }
 }
