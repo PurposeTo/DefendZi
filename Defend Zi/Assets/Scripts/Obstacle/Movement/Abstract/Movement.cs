b9827b730b23a7e8f01f417007426fcd38a5f624
AlexeyR1 <repin.alex2099@yandex.ru>
2021-06-18 02:16:33 +0300

    Revert "Update Movement.cs"
    This reverts commit 6e5635c0f3dde38c27cd749321f2143151a13662.

--- Defend Zi/Assets/Scripts/Obstacle/Movement/Abstract/Movement.cs
+++ Defend Zi/Assets/Scripts/Obstacle/Movement/Abstract/Movement.cs
@@ -6,6 +6,8 @@ public abstract class Movement : MonoBehaviourExt
     [SerializeField] private float _speed;
     protected float Speed => _speed;
 
+    protected float Counter;
+
     private void FixedUpdate()
     {
         Move(Time.fixedDeltaTime);
