518ef4859beffeff66c6a61a6e52e8b7bf92bbf3
AlexeyR1 <repin.alex2099@yandex.ru>
2021-06-18 18:35:11 +0300

    Some fixes

--- Defend Zi/Assets/Scripts/Obstacle/Movement/HorizontalMovement.cs
+++ Defend Zi/Assets/Scripts/Obstacle/Movement/HorizontalMovement.cs
@@ -5,7 +5,7 @@ public class HorizontalMovement : PositionMoverMono
     private void FixedUpdate()
     {
         Debug.Log($"{GetType()}. PreviousPos: {Position.Value.x}, {Position.Value.y}");
-        Position.MoveBy(new Vector2(Speed * Time.fixedDeltaTime, 0f));
+        Position.MoveBy(Vector2.right * Speed * Time.fixedDeltaTime);
         Debug.Log($"{GetType()}. NextPos: {Position.Value.x}, {Position.Value.y}");
     }
 }
--- Defend Zi/Assets/Scripts/Obstacle/Movement/VerticalMovement.cs
+++ Defend Zi/Assets/Scripts/Obstacle/Movement/VerticalMovement.cs
@@ -5,7 +5,7 @@ public class VerticalMovement : PositionMoverMono
     private void FixedUpdate()
     {
         Debug.Log($"{GetType()}. PreviousPos: {Position.Value.x}, {Position.Value.y}");
-        Position.MoveBy(new Vector2(0f, Speed * Time.fixedDeltaTime));
+        Position.MoveBy(Vector2.up * Speed * Time.fixedDeltaTime);
         Debug.Log($"{GetType()}. NextPos: {Position.Value.x}, {Position.Value.y}");
     }
 }
