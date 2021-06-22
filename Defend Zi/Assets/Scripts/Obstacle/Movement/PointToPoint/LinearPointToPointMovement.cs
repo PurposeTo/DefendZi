b88234f54ab892f58e5593dfb2fa91a6e05bc0c6
AlexeyR1 <repin.alex2099@yandex.ru>
2021-06-22 21:06:51 +0300

    Add point to point movement

---
+++ Defend Zi/Assets/Scripts/Obstacle/Movement/PointToPoint/LinearPointToPointMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using Desdiene.MonoBehaviourExtension;
+
+public class LinearPointToPointMovement : PointToPointMovement
+{
+    public LinearPointToPointMovement(MonoBehaviourExt monoBehaviour,
+                                      IPosition position,
+                                      Vector2 targetPosition,
+                                      float speed) : base(monoBehaviour, position, targetPosition, speed) { }
+
+    protected override IEnumerator Move()
+    {
+        float counter = 0f;
+        var startPosition = Position.Value;
+        var wait = new WaitForFixedUpdate();
+
+        while (counter <= 1f)
+        {
+            Position.MoveTo(Vector2.Lerp(startPosition, TargetPosition, counter));
+            counter += Time.fixedDeltaTime * Speed;
+            yield return wait;
+        }
+
+        Position.MoveTo(TargetPosition);
+    }
+}
---
+++ Defend Zi/Assets/Scripts/Obstacle/Movement/PointToPoint/PointToPointMovementMono.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+public class PointToPointMovementMono : PositionMoverMono
+{
+    private enum MovementType
+    {
+        Linear,
+        NonLinear
+    }
+
+    [SerializeField] private Transform _targetTransform;
+    [SerializeField] private MovementType _movementType;
+    [SerializeField] private AnimationCurve _animationCurve;
+
+    private PointToPointMovement _movement;
+
+    protected override void Constructor()
+    {
+        base.Constructor();
+        InitializeMovement();
+    }
+
+    // TODO: Убрать (добавлено для тестирования)
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _movement.Enable();
+        }
+    }
+
+    private void InitializeMovement()
+    {
+        if (_movementType == MovementType.Linear)
+        {
+            _movement = new LinearPointToPointMovement(this, Position, _targetTransform.position, Speed);
+        }
+        else if (_movementType == MovementType.NonLinear)
+        {
+            _movement = new NonLinearPointToPointMovement(this, Position, _targetTransform.position, _animationCurve, Speed);
+        }
+        else throw new Exception($"{_movementType} is unkwown type of {typeof(MovementType)}.");
+    }
+}
