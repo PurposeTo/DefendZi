f080e8bc3a5a1b10b9b0f275a03a54892c5c20b2
AlexeyR1 <repin.alex2099@yandex.ru>
2021-06-22 16:28:25 +0300

    Deleted movements

--- Defend Zi/Assets/Scripts/Obstacle/Movement/HorizontalMovement.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class HorizontalMovement : PositionMoverMono
-{
-    private void FixedUpdate()
-    {
-        Position.MoveBy(Vector2.right * Speed * Time.fixedDeltaTime);
-    }
-}
--- Defend Zi/Assets/Scripts/Obstacle/Movement/VerticalMovement.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class VerticalMovement : PositionMoverMono
-{
-    private void FixedUpdate()
-    {
-        Position.MoveBy(Vector2.up * Speed * Time.fixedDeltaTime);
-    }
-}
