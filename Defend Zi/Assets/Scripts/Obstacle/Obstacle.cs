70aa2ae4dde92c247b440edb0931f1c8c263626d
AlexeyR1 <repin.alex2099@yandex.ru>
2021-06-25 19:36:39 +0300

    Realize IPosition by ObstacleMono

--- Defend Zi/Assets/Scripts/Obstacle/Obstacle.cs
+++ Defend Zi/Assets/Scripts/Obstacle/Obstacle.cs
@@ -1,17 +1,40 @@
 using System;
 using UnityEngine;
 
-public class Obstacle : IDamageDealer, IScoreGetter
+public class Obstacle :
+    IDamageDealer,
+    IScoreGetter,
+    IPosition
 {
     private readonly IDamageDealer _damage = new Damage();
     private readonly IScoreGetter _score;
+    private readonly IPosition _position;
 
-    public Obstacle(int scoreByAvoding)
+    public Obstacle(int scoreByAvoding, Rigidbody2D rigidbody2D)
     {
         _score = new ScoreGetter(scoreByAvoding);
+        _position = new Position(rigidbody2D);
     }
+
+    uint IDamageDealer.Value => _damage.Value;
 
     int IScoreGetter.Value => _score.Value;
 
-    uint IDamageDealer.Value => _damage.Value;
+    Vector2 IPositionGetter.Value => _position.Value;
+
+    void IMovePosition.MoveBy(Vector2 deltaDistance)
+    {
+        _position.MoveBy(deltaDistance);
+    }
+
+    void IMovePosition.MoveTo(Vector2 finalPosition)
+    {
+        _position.MoveTo(finalPosition);
+    }
+
+    event Action IPositionNotification.OnChanged
+    {
+        add => _position.OnChanged += value;
+        remove => _position.OnChanged -= value;
+    }
 }
--- Defend Zi/Assets/Scripts/Obstacle/ObstacleMono.cs
+++ Defend Zi/Assets/Scripts/Obstacle/ObstacleMono.cs
@@ -1,26 +1,49 @@
+using System;
 using Desdiene.MonoBehaviourExtension;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[DisallowMultipleComponent]
 public class ObstacleMono : 
     MonoBehaviourExt, 
     IDamageDealer, 
-    IScoreGetter
+    IScoreGetter,
+    IPosition
 {
+    [SerializeField] private int _scoreByAvoding = 5;
+
     private IScoreGetter _scoreGetter;
     private IDamageDealer _damageDealer;
+    private IPosition _position;
 
     protected override void Constructor()
     {
-        Obstacle obstacle = new Obstacle(_scoreByAvoding);
+        Obstacle obstacle = new Obstacle(_scoreByAvoding, GetComponent<Rigidbody2D>());
 
         _scoreGetter = obstacle;
         _damageDealer = obstacle;
+        _position = obstacle;
     }
 
-    [SerializeField] private int _scoreByAvoding = 5;
+    uint IDamageDealer.Value => _damageDealer.Value;
 
     int IScoreGetter.Value => _scoreGetter.Value;
 
-    uint IDamageDealer.Value => _damageDealer.Value;
+    Vector2 IPositionGetter.Value => _position.Value;
+
+    void IMovePosition.MoveTo(Vector2 finalPosition)
+    {
+        _position.MoveTo(finalPosition);
+    }
+
+    void IMovePosition.MoveBy(Vector2 deltaDistance)
+    {
+        _position.MoveBy(deltaDistance);
+    }
+
+    event Action IPositionNotification.OnChanged
+    {
+        add { _position.OnChanged += value; }
+        remove { _position.OnChanged -= value; }
+    }
 }
