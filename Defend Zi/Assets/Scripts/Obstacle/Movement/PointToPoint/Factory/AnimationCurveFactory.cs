018a81d49a79f6d514b23d0c0dca15fdb595e63f
AlexeyR1 <repin.alex2099@yandex.ru>
2021-06-28 11:28:04 +0300

    Update AnimationCurveFactory.cs

--- Defend Zi/Assets/Scripts/Obstacle/Movement/PointToPoint/Factory/AnimationCurveFactory.cs
+++ Defend Zi/Assets/Scripts/Obstacle/Movement/PointToPoint/Factory/AnimationCurveFactory.cs
@@ -18,19 +18,19 @@ public static class AnimationCurveFactory
         AddEntry(CurveType.EaseInOut, AnimationCurve.EaseInOut(0f, 0f, 1f, 1f));
     }
 
-    public static AnimationCurve Get(CurveType movementType)
+    public static AnimationCurve Get(CurveType curveType)
     {
-        return _curves.TryGetValue(movementType, out var value)
+        return _curves.TryGetValue(curveType, out var value)
             ? value
-            : throw new InvalidOperationException($"Value by key {movementType} does not exist in {_curves}");
+            : throw new InvalidOperationException($"Value by key {curveType} does not exist in {_curves}");
     }
 
-    private static void AddEntry(CurveType movementType,
+    private static void AddEntry(CurveType curveType,
                                  AnimationCurve curve)
     {
-        if (_curves.ContainsKey(movementType)) throw new InvalidOperationException($"{_curves} already contains {movementType} key.");
+        if (_curves.ContainsKey(curveType)) throw new InvalidOperationException($"{_curves} already contains {curveType} key.");
         if (curve is null) throw new ArgumentNullException(nameof(curve));
 
-        _curves.Add(movementType, curve);
+        _curves.Add(curveType, curve);
     }
 }
