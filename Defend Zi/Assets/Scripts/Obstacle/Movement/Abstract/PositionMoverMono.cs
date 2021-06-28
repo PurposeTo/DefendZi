953198a36106921b0560173926e730035510b960
AlexeyR1 <repin.alex2099@yandex.ru>
2021-06-28 05:51:58 +0300

    Update PositionMoverMono.cs

--- Defend Zi/Assets/Scripts/Obstacle/Movement/Abstract/PositionMoverMono.cs
+++ Defend Zi/Assets/Scripts/Obstacle/Movement/Abstract/PositionMoverMono.cs
@@ -4,7 +4,7 @@ using Desdiene.MonoBehaviourExtension;
 public abstract class PositionMoverMono : MonoBehaviourExt
 {
     [SerializeField] private float _speed;
-    [SerializeField, NotNull] private InterfaceComponent<IPosition> _position;
+    [SerializeField, NotNull] private IPositionComponent _position;
 
     protected float Speed => _speed;
     protected IPosition Position => _position.Implementation;
