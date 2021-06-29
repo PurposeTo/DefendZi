d3009a9f4153b399d1839b8acc401bac029f9b4c
AlexeyR1 <repin.alex2099@yandex.ru>
2021-06-29 03:13:33 +0300

    Update chunk size visualizing

--- Defend Zi/Assets/Scripts/LevelGenerator/Chunk/Base/Chunk.cs
+++ Defend Zi/Assets/Scripts/LevelGenerator/Chunk/Base/Chunk.cs
@@ -1,11 +1,33 @@
 ﻿using Desdiene.MonoBehaviourExtension;
 using UnityEngine;
+using Desdiene;
 
 public abstract class Chunk : MonoBehaviourExt
 {
-    [SerializeField] private float _width;
     [SerializeField] private float _height;
+    [SerializeField] private float _width;
+    [SerializeField] private float _minWidth;
+    [SerializeField] private float _maxWidth;
 
-    public float Width => _width;
     public float Height => _height;
+    public float Width => _width;
+    public float MinWidth => _minWidth;
+    public float MaxWidth => _maxWidth;
+
+    protected override void Constructor()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        _height = Mathf.Abs(_height);
+        _width = Mathf.Abs(_width);
+        _minWidth = Mathf.Abs(_minWidth);
+        _maxWidth = Mathf.Abs(_maxWidth);
+
+        if (_maxWidth < _minWidth) Randomizer.Swap(ref _maxWidth, ref _minWidth);
+
+        _width = Mathf.Clamp(_width, _minWidth, _maxWidth);
+    }
 }
