ad42d54e116ba93b0b1f2db67a31235c109cb672
AlexeyR1 <repin.alex2099@yandex.ru>
2021-06-29 18:59:44 +0300

    Update Chunk.cs

--- Defend Zi/Assets/Scripts/LevelGenerator/Chunk/Base/Chunk.cs
+++ Defend Zi/Assets/Scripts/LevelGenerator/Chunk/Base/Chunk.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public abstract class Chunk : MonoBehaviourExt
 {
     // TODO: заменить на ValueInRange
-    [Min(0f)] private float width;
+    [Min(0f)] private float _width;
 
     [SerializeField, Min(0f)] private float _height = 20;
 
@@ -12,14 +12,14 @@ public abstract class Chunk : MonoBehaviourExt
     [SerializeField, Min(0f)] private float _minWidth;
     [SerializeField, Min(0f)] private float _maxWidth;
 
-    public float Width => width;
+    public float Width => _width;
     public float Height => _height;
     public float MinWidth => _minWidth;
     public float MaxWidth => _maxWidth;
 
     private void Awake()
     {
-        width = Random.Range(_minWidth, _maxWidth);
+        _width = Random.Range(_minWidth, _maxWidth);
     }
 
     private void Start()
