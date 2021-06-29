89737cc2a02242fddb22aba9f324817b2e7f1e9c
AlexeyR1 <repin.alex2099@yandex.ru>
2021-06-29 07:10:41 +0300

    Add algorithm

--- Defend Zi/Assets/Scripts/LevelGenerator/Chunk/TwoSquads.cs
+++ Defend Zi/Assets/Scripts/LevelGenerator/Chunk/TwoSquads.cs
@@ -6,8 +6,8 @@ public class TwoSquads : Chunk
     [SerializeField, NotNull] private GameObject _squadFirst;
     [SerializeField, NotNull] private GameObject _squadSecond;
 
-    private readonly float[] hights = { -7, -4.5f, -3.75f, -3, -2, -1, 0, 1, 2, 3, 3.75f, 4.5f, 7 };
-    private readonly BestRotationEulers bestRotationEulers = new BestRotationEulers();
+    private readonly float[] _hights = { -7, -4.5f, -3.75f, -3, -2, -1, 0, 1, 2, 3, 3.75f, 4.5f, 7 };
+    private readonly BestRotationEulers _bestRotationEulers = new BestRotationEulers();
 
     private void Start()
     {
@@ -17,15 +17,18 @@ public class TwoSquads : Chunk
     private void OnSpawn()
     {
         //todo: Высота. Выбрать два случайных НЕ рядом стоящих числа. 
+        float[] hights = new TwoNonAdjacentNumbers().Get(_hights);
+        print($"1: {hights[0]}");
+        print($"2: {hights[1]}");
 
         _squadFirst.transform
             .SetPositionOy(GetRandomHight())
-            .SetRotation(Quaternion.Euler(0f, 0f, bestRotationEulers.GetRandom()));
+            .SetRotation(Quaternion.Euler(0f, 0f, _bestRotationEulers.GetRandom()));
 
         _squadSecond.transform
             .SetPositionOy(GetRandomHight())
-            .SetRotation(Quaternion.Euler(0f, 0f, bestRotationEulers.GetRandom()));
+            .SetRotation(Quaternion.Euler(0f, 0f, _bestRotationEulers.GetRandom()));
     }
 
-    private float GetRandomHight() => hights[Random.Range(0, hights.Length)];
+    private float GetRandomHight() => _hights[Random.Range(0, _hights.Length)];
 }
---
+++ Defend Zi/Assets/Scripts/LevelGenerator/TwoNonAdjacentNumbers.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class TwoNonAdjacentNumbers
+{
+    private float _first;
+    private float _second;
+    private int _firstIndex;
+
+    public float[] Get(float[] sourceArray)
+    {
+        InitFirst(sourceArray, out bool isLeftNeighborExists, out bool isRightNeighborExists);
+        InitSecond(sourceArray, isLeftNeighborExists, isRightNeighborExists);
+        return new float[] { _first, _second };
+    }
+
+    private void InitFirst(float[] sourceArray, out bool isLeftNeighborExists, out bool isRightNeighborExists)
+    {
+        _firstIndex = Random.Range(0, sourceArray.Length);
+        _first = sourceArray[_firstIndex];
+
+        isLeftNeighborExists = _firstIndex > 0;
+        isRightNeighborExists = _firstIndex < sourceArray.Length - 1;
+    }
+
+    private void InitSecond(float[] sourceArray, bool isLeftNeighborExists, bool isRightNeighborExists)
+    {
+        float[] nonAdjacentToFirst = GetNonAdjacentToFirstNumbers(sourceArray, isLeftNeighborExists, isRightNeighborExists);
+        _second = nonAdjacentToFirst[Random.Range(0, nonAdjacentToFirst.Length)];
+    }
+
+    private float[] GetNonAdjacentToFirstNumbers(float[] sourceArray, bool isLeftNeighborExists, bool isRightNeighborExists)
+    {
+        int counter = 0;
+        float[] nonAdjacentToFirst;
+        System.Predicate<int> predicate = new System.Predicate<int>((i) =>
+        {
+            return (i == _firstIndex) || (isLeftNeighborExists && i == _firstIndex - 1) || (isRightNeighborExists && i == _firstIndex + 1);
+        });
+
+        if (isLeftNeighborExists && isRightNeighborExists)
+        {
+            nonAdjacentToFirst = new float[sourceArray.Length - 3];
+        }
+        else nonAdjacentToFirst = new float[sourceArray.Length - 2];
+
+        for (int i = 0; i < sourceArray.Length; i++)
+        {
+            if (predicate(i)) continue;
+            nonAdjacentToFirst[counter] = sourceArray[i];
+            counter++;
+        }
+
+        return nonAdjacentToFirst;
+    }
+}
