064523db3643a549974afed7628c5f3acba9bfdb
PurposeTo <firecrystal1999@gmail.com>
2021-06-29 18:39:17 +0300

    Update balance

--- Defend Zi/Assets/Scripts/Player/PlayerMovementData.cs
+++ Defend Zi/Assets/Scripts/Player/PlayerMovementData.cs
@@ -6,7 +6,7 @@ public class PlayerMovementData
 {
     [SerializeField] public float speed = 12f;
     [SerializeField] public float amplitude = 6f;
-    [SerializeField] public float defaultFrequency = 0.125f;
+    [SerializeField] public float defaultFrequency = 0.1f;
     [SerializeField] public float controlledFrequency = 0.5f;
     [SerializeField] public float frequencyChangeRate = 1.5f;
 }
