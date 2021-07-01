ec4c20050a8e230be0c553059bab881ef1fa24f6
AlexeyR1 <repin.alex2099@yandex.ru>
2021-07-01 11:44:02 +0300

    Update InterfaceComponent.cs

--- Defend Zi/Assets/Scripts/Interface components/Abstract/InterfaceComponent.cs
+++ Defend Zi/Assets/Scripts/Interface components/Abstract/InterfaceComponent.cs
@@ -3,12 +3,14 @@
 // В дочерних классах добавить GetComponent()
 public abstract class InterfaceComponent<T> : MonoBehaviourExt where T : class
 {
-    public T Implementation => _implementation;
+    public T Implementation
+    {
+        get
+        {
+            if (_implementation == null) _implementation = GetComponent<T>();
+            return _implementation;
+        }
+    }
 
     private T _implementation;
-
-    protected override void AwakeExt()
-    {
-        _implementation = GetComponent<T>();
-    }
 }
