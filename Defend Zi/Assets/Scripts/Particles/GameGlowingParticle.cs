4dcc2238c6c8c5d459cc6d1af292afb723cb0be3
PurposeTo <firecrystal1999@gmail.com>
2021-09-16 01:57:37 +0300

    Update glowing particles

---
+++ Defend Zi/Assets/Scripts/Particles/GameGlowingParticle.cs
@@ -0,0 +1,53 @@
+﻿using Desdiene.MonoBehaviourExtension;
+using Desdiene.TimeControls.Scalers;
+using Desdiene.Types.Processes;
+using UnityEngine;
+using Zenject;
+
+public class GameGlowingParticle : MonoBehaviourExt
+{
+    [SerializeField, NotNull] private GlowingParticle _glowing;
+    private IProcessAccessorNotifier _globalPause;
+
+    [Inject]
+    private void Constructor(GlobalTimeScaler globalTimeScaler)
+    {
+        _globalPause = globalTimeScaler ?? throw new System.ArgumentNullException(nameof(globalTimeScaler));
+    }
+
+    protected override void AwakeExt()
+    {
+        SubscribeEvents();
+    }
+
+    protected override void OnDisableExt()
+    {
+        UnsubscribeEvents();
+    }
+
+    private float GlowingSpeedOx => -12;
+
+    private void SubscribeEvents()
+    {
+        _globalPause.OnStarted += StartGlowingMotion;
+        _globalPause.OnCompleted += StopGlowingMotion;
+    }
+
+    private void UnsubscribeEvents()
+    {
+        _globalPause.OnStarted -= StartGlowingMotion;
+        _globalPause.OnCompleted -= StopGlowingMotion;
+    }
+
+    private void StartGlowingMotion()
+    {
+        Debug.Log("КРЯ Start");
+        _glowing.SetConstantSpeedOx(GlowingSpeedOx);
+    }
+
+    private void StopGlowingMotion()
+    {
+        Debug.Log("КРЯ Stop");
+        _glowing.SetConstantSpeedOx(0f);
+    }
+}
---
+++ Defend Zi/Assets/Scripts/Particles/GlowingParticle.cs
@@ -0,0 +1,16 @@
+﻿using Desdiene.MonoBehaviourExtension;
+using UnityEngine;
+
+/// <summary>
+/// Описывает систему частиц "Glowing particle" как модель.
+/// </summary>
+public class GlowingParticle : MonoBehaviourExt
+{
+    [SerializeField, NotNull] private ParticleSystem _particleSystem;
+
+    public void SetConstantSpeedOx(float speed)
+    {
+        ParticleSystem.VelocityOverLifetimeModule velocity = _particleSystem.velocityOverLifetime;
+        velocity.x = new ParticleSystem.MinMaxCurve(speed);
+    }
+}
