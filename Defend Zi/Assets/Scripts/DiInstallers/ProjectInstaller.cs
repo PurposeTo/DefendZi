8245ab4ca6e3ca4512e1fa90c91bf985ed6d51fc
PurposeTo <firecrystal1999@gmail.com>
2021-09-27 23:59:54 +0300

    Add admob

--- Defend Zi/Assets/Scripts/DiInstallers/ProjectInstaller.cs
+++ Defend Zi/Assets/Scripts/DiInstallers/ProjectInstaller.cs
@@ -11,6 +11,7 @@ public class ProjectInstaller : MonoInstaller
 {
     public override void InstallBindings()
     {
+        BindRewardedAd();
         BindFullScreenWindowsContainer();
         BindScenesInBuild();
         BindLoadedScenes();
@@ -19,6 +20,16 @@ public class ProjectInstaller : MonoInstaller
         BingSingleSceneLoader();
         BindGPGSAuthentication();
         BindDataStorage();
+    }
+
+    private void BindRewardedAd()
+    {
+        Container
+            .Bind<IRewardedAd>()
+            .To<GoogleAdMob>()
+            .FromNewComponentOnNewGameObject()
+            .AsSingle()
+            .NonLazy();
     }
 
     private void BindFullScreenWindowsContainer()
