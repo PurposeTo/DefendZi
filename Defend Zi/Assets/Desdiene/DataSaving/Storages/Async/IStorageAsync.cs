3c665d541d09c8833ff92423cf1e8ae5ea7ccd46
PurposeTo <firecrystal1999@gmail.com>
2021-10-21 17:17:11 +0300

    Update packages

---
+++ Defend Zi/Assets/Desdiene/DataSaving/Storages/Async/IStorageAsync.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Desdiene.DataSaving.Storages
+{
+    /// <summary>
+    /// Асинхронное хранилище данных.
+    /// 
+    /// НЕ использовать объект с полученными данными как модель в игровой логике! 
+    /// Использовать лишь как data transfer object.
+    /// </summary>
+    /// <typeparam name="T">Объект с данными</typeparam>
+    public interface IStorageAsync<T>
+    {
+        string StorageName { get; }
+
+        void Load(Action<bool, T> result);
+        void Save(T data, Action<bool> successResult);
+        void Clean(Action<bool> successResult);
+    }
+}
---
+++ Defend Zi/Assets/Desdiene/DataSaving/Storages/Sync/IStorage.cs
@@ -0,0 +1,24 @@
+﻿namespace Desdiene.DataSaving.Storages
+{
+    /// <summary>
+    /// Хранилище данных.
+    /// 
+    /// НЕ использовать объект с полученными данными как модель в игровой логике! 
+    /// Использовать лишь как data transfer object.
+    /// </summary>
+    /// <typeparam name="T">Объект с данными</typeparam>
+    public interface IStorage<T>
+    {
+        string StorageName { get; }
+
+        /// <returns>успешно?</returns>
+        bool TryLoad(out T data);
+
+
+        /// <returns>успешно?</returns>
+        bool Save(T data);
+
+        /// <returns>успешно?</returns>
+        bool TryClean();
+    }
+}
