b3088c00c6b675a9c428f28670c2fc24d52a92cd
PurposeTo <firecrystal1999@gmail.com>
2021-10-25 22:01:54 +0300

    Update IDataWithTotalInAppTime.cs

--- Defend Zi/Assets/Desdiene/DataSaving/Datas/IDataWithTotalInAppTime.cs
+++ Defend Zi/Assets/Desdiene/DataSaving/Datas/IDataWithTotalInAppTime.cs
@@ -2,6 +2,9 @@
 
 namespace Desdiene.DataSaving.Datas
 {
+    /// <summary>
+    /// Необходим для разрешения конфликтов при взаимодействии с хранилищем через выбор данных с самым большим значением TotalInAppTime
+    /// </summary>
     public interface IDataWithTotalInAppTime
     {
         TimeSpan TotalInAppTime { get; }
