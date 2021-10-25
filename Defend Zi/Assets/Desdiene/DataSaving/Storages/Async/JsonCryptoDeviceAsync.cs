d1b2cc1e51fbfdd55d88881c0b0e8ea7f7acb639
PurposeTo <firecrystal1999@gmail.com>
2021-10-25 20:47:20 +0300

    Add JsonCryptoDeviceAsync

---
+++ Defend Zi/Assets/Desdiene/DataSaving/Storages/Async/JsonCryptoDeviceAsync.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Assets.Desdiene.Tools;
+using Desdiene.DataSaving.Datas;
+using Desdiene.Json;
+using Desdiene.MonoBehaviourExtension;
+using Desdiene.Tools;
+
+namespace Desdiene.DataSaving.Storages
+{
+    /// <summary>
+    /// Сохранить файл в json формате на устройство.
+    /// 
+    /// </summary>
+    /// <typeparam name="T">Объект с данными, загружаемый/сохраняемый в хранилище.</typeparam>
+    public class JsonCryptoDeviceAsync<T> : JsonStorageAsync<T> where T : IJsonSerializable, IValidData
+    {
+        protected readonly string _filePath;
+        protected readonly DeviceDataReader _deviceDataReader;
+
+        public JsonCryptoDeviceAsync(MonoBehaviourExt mono, string baseFileName, IJsonDeserializer<T> jsonDeserializer)
+            : base("Асинхронное хранилище зашифрованных Json данных на устройстве",
+                  baseFileName,
+                  jsonDeserializer)
+        {
+            if (mono == null) throw new ArgumentNullException(nameof(mono));
+
+            _filePath = new FilePath(FileName).Value;
+            _deviceDataReader = new DeviceDataReader(mono, _filePath);
+        }
+
+        protected override void ReadJson(Action<bool, string> result)
+        {
+            _deviceDataReader.Read((success, jsonData) =>
+            {
+                if(success && string.IsNullOrWhiteSpace(jsonData))
+                {
+                    jsonData = EmptyJson;
+                }
+
+                result?.Invoke(success, jsonData);
+            });
+        }
+
+        protected sealed override void UpdateJson(string jsonData, Action<bool> successResult)
+        {
+            // try-catch исключений происходит в родительском классе.
+            DeviceFile.WriteAllText(_filePath, jsonData);
+            successResult?.Invoke(true);
+        }
+
+        protected sealed override void DeleteData(Action<bool> successResult)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
