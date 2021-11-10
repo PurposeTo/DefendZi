39d901fb0354202c8c3fde093711876ff1f1ad76
PurposeTo <firecrystal1999@gmail.com>
2021-11-10 17:12:53 +0300

    Update GPGSLeaderboard.cs

--- Defend Zi/Assets/Desdiene/GooglePlayApi/GPGSLeaderboard.cs
+++ Defend Zi/Assets/Desdiene/GooglePlayApi/GPGSLeaderboard.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
 
 namespace Desdiene.GooglePlayApi
 {
@@ -8,6 +10,7 @@ namespace Desdiene.GooglePlayApi
         private readonly string _leaderboardId;
         private readonly PlayGamesPlatform _platform;
         private long _cashScore = -1; // По умолчанию -1, если счет не был успешно обновлен в таблице лидеров
+        private bool _isCashScoreInited = false;
 
         public GpgsLeaderboard(PlayGamesPlatform platform, string leaderboardId)
         {
@@ -22,14 +25,28 @@ namespace Desdiene.GooglePlayApi
 
         public void Open()
         {
-            _platform.ShowLeaderboardUI(_leaderboardId);
+            _platform.ShowLeaderboardUI(_leaderboardId, (status) =>
+            {
+                Debug.Log($"{GetType().Name} opened with status {status}");
+            });
         }
 
         public void UpdateScore(long score, Action<bool> result)
         {
+            if (_isCashScoreInited && _cashScore == score)
+            {
+                result?.Invoke(true);
+                return;
+            }
+
             _platform.ReportScore(score, _leaderboardId, (success) =>
             {
-                if (success) _cashScore = score;
+                if (success)
+                {
+                    _isCashScoreInited = true;
+                    _cashScore = score;
+                }
+
                 result?.Invoke(success);
             });
         }
