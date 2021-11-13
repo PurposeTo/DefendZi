56cd19a095489db505a37c9b8da9976d4a6b732d
PurposeTo <firecrystal1999@gmail.com>
2021-11-13 19:40:44 +0300

    Update training image

---
+++ Defend Zi/Assets/Desdiene/UI/Elements/CutOutMaskUi.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+namespace Desdiene.UI.Elements
+{
+    public class CutOutMaskUi : Image
+    {
+        public override Material materialForRendering
+        {
+            get
+            {
+                Material material = new Material(base.materialForRendering);
+                material.SetInt("_StencilComp", (int)CompareFunction.NotEqual);
+                return material;
+            }
+        }
+    }
+}
