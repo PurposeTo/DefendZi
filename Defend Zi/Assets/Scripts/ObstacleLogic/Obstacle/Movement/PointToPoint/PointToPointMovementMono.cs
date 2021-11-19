e2b75135c05217edca1ef1bb38595ad9a75d92a8
PurposeTo <firecrystal1999@gmail.com>
2021-11-19 19:56:06 +0300

    Update point to point chunk

--- Defend Zi/Assets/Scripts/ObstacleLogic/Obstacle/Movement/PointToPoint/PointToPointMovementMono.cs
+++ Defend Zi/Assets/Scripts/ObstacleLogic/Obstacle/Movement/PointToPoint/PointToPointMovementMono.cs
@@ -1,23 +1,16 @@
-﻿using Desdiene.MonoBehaviourExtension;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PointToPointMovementMono : PositionMoverMono, ITriggerable
 {
-    [SerializeField, NotNull] private InterfaceComponent<IPosition> _target;
     [SerializeField] private AnimationCurveFactory.CurveType _curveType;
 
     private PointToPointMovement _movement;
 
-    protected override void AwakeExt()
+    public void Init(IPositionAccessor target)
     {
-        Init();
+        var animationCurve = AnimationCurveFactory.Get(_curveType);
+        _movement = new PointToPointMovement(this, Position, target, Speed, animationCurve);
     }
 
     void ITriggerable.Invoke() => _movement.Move();
-
-    private void Init()
-    {
-        var animationCurve = AnimationCurveFactory.Get(_curveType);
-        _movement = new PointToPointMovement(this, Position, _target.Implementation, Speed, animationCurve);
-    }
 }
