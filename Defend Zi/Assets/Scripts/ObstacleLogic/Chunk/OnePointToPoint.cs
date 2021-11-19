0db846f42b5322c1fa5cb23c7dead2079d8fc316
PurposeTo <firecrystal1999@gmail.com>
2021-11-19 19:57:28 +0300

    Update OnePointToPoint.cs

--- Defend Zi/Assets/Scripts/ObstacleLogic/Chunk/OnePointToPoint.cs
+++ Defend Zi/Assets/Scripts/ObstacleLogic/Chunk/OnePointToPoint.cs
@@ -14,7 +14,6 @@ public class OnePointToPoint : Chunk, ITriggerable
         Quaternion rotation = Quaternion.Euler(0f, 0f, angle);
 
         Vector2 toTarget = (Vector2)_obstacle.transform.position - Target.Value;
-        Debug.Log($"ÊÐß {(Vector2)_obstacle.transform.position} - {Target.Value} = {toTarget}");
         toTarget = rotation * toTarget;
         Target.MoveTo((Vector2)_obstacle.transform.position + toTarget);
 
