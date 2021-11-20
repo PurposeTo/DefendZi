0b9699818197e91b60685274b21c292bf598914c
PurposeTo <firecrystal1999@gmail.com>
2021-11-20 17:46:20 +0300

    Update point to point chunk

--- Defend Zi/Assets/Scripts/ObstacleLogic/Obstacle/Movement/PointToPoint/PointToPointMovementMono.cs
+++ Defend Zi/Assets/Scripts/ObstacleLogic/Obstacle/Movement/PointToPoint/PointToPointMovementMono.cs
@@ -9,6 +9,12 @@ public class PointToPointMovementMono : PositionMoverMono, ITriggerable
     public void Init(IPositionAccessor target)
     {
         var animationCurve = AnimationCurveFactory.Get(_curveType);
+
+        // установка случайной скорости.
+        // todo: какая сущность должна заниматься параметризацией этих значений? у некоторых сущностей PositionMoverMono она - константа. У других - Range между двумя случайными значениями. Плюс завадаться она должна из инспектора или из кода?
+        float speed = Random.Range(Speed * 0.5f, Speed * 1.5f);
+        SetSpeed(speed);
+
         _movement = new PointToPointMovement(this, Position, target, Speed, animationCurve);
     }
 
