b9baa6e9eae5d526ce9ed69235b7df83eb456d88
AlexeyR1 <repin.alex2099@yandex.ru>
2021-11-20 09:50:44 +0300

    Update UI

--- Defend Zi/Assets/Scripts/UI/PopUpError/ModalError.cs
+++ Defend Zi/Assets/Scripts/UI/PopUpError/ModalError.cs
@@ -7,13 +7,13 @@ public class ModalError : ModalWindow
 {
     [SerializeField, NotNull] private TextView _message;
     private Button _closeButton;
-    //  [SerializeField, NotNull] private Button _mainMenuButton;
+    [SerializeField, NotNull] private Button _mainMenuButton;
 
     protected override void AwakeWindow()
     {
         _closeButton = GetComponent<Button>();
         _closeButton.onClick.AddListener(() => Hide());
-        //    _mainMenuButton.onClick.AddListener(() => OnMainMenuClicked?.Invoke());
+        _mainMenuButton.onClick.AddListener(() => OnMainMenuClicked?.Invoke());
     }
 
     public void Init(string message)
